d2872cc6cb1bbf55fe7b726dc11b41a8635eb4be
Suraj Karki <suraj.karki500@gmail.com>
2020-09-13 12:09:44 +0545

    Finished word frequencies

--- Logistic-Regression/Sentiment-Analysis/utils.py
+++ Logistic-Regression/Sentiment-Analysis/utils.py
@@ -31,3 +31,22 @@ def process_tweet(tweet):
             stem_word = stemmer.stem(word)
             clean_tweets.append(stem_word)
     return clean_tweets
+
+def build_freqs(tweets, ys):
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+
+    return freqs
